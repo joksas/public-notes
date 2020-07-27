1f236359edf4a15044a25dd418150edae1370790
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-07-27 16:11:52 +0300

    create

--- programming/languages/go/The-Go-Programming-Language/exercises/src/ch1/exercises/ex-1-4/main.go
+++ programming/languages/go/The-Go-Programming-Language/exercises/src/ch1/exercises/ex-1-4/main.go
@@ -7,7 +7,7 @@
 // Dup2 prints the count and text of lines that appear more than once
 // in the input.  It reads from stdin or from a list of named files.
 
-// I know this solution is inefficient. It is just that I don't know the syntax that well yet to use my preferred solution (array of arrays).Yoshke.
+// I know this solution is inefficient. It is just that I don't know the syntax that well yet to use my preferred solution (array of arrays). Yoshke.
 
 package main
 
