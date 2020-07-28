2f23eae80219cf885a438f47a6d7bebfff91de4c
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-07-28 19:40:40 +0300

    complete execise

---
+++ programming/languages/go/The-Go-Programming-Language/practise/my-solutions/ch1/ex-1-10/main.go
@@ -0,0 +1,63 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 17.
+//!+
+
+// Fetchall fetches URLs in parallel and reports their times and sizes.
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	f, err := os.Create("output.txt")
+	check(err)
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		n, err := f.WriteString(<-ch + "\n")
+		check(err)
+		fmt.Printf("wrote %d bytes\n", n)
+	}
+	summary := fmt.Sprintf("%.2fs elapsed\n", time.Since(start).Seconds())
+	n, err := f.WriteString(summary)
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n)
+	f.Close()
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+//!-
